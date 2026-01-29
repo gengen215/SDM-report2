43365cb2c1928b9119d3dfbfe6f7491206f0c9eb
WAKATSUKI Genta <g253388@ydev04.edu.tut.ac.jp>
2026-01-29 16:50:21 +0900

    Add tests based on equivalence partitioning and boundary values

--- test_cases.py
+++ test_cases.py
@@ -18,4 +18,39 @@ class TestCalc (unittest.TestCase):
 
         def test_sample4 (self):
                 self.assertEqual (-1, calc(0.1,999))
+        # ---- boundary / valid ----
+        def test_valid_1_1 (self):
+                self.assertEqual (1, calc(1,1))
+
+        def test_valid_1_999 (self):
+                self.assertEqual (999, calc(1,999))
+
+        def test_valid_999_1 (self):
+                self.assertEqual (999, calc(999,1))
+
+        def test_valid_999_999 (self):
+                self.assertEqual (998001, calc(999,999))
+
+        # ---- boundary / invalid range ----
+        def test_invalid_0_1 (self):
+                self.assertEqual (-1, calc(0,1))
+
+        def test_invalid_1_0 (self):
+                self.assertEqual (-1, calc(1,0))
+
+        def test_invalid_1000_1 (self):
+                self.assertEqual (-1, calc(1000,1))
+
+        def test_invalid_1_1000 (self):
+                self.assertEqual (-1, calc(1,1000))
+
+        # ---- invalid type (non-integer) ----
+        def test_invalid_1_float (self):
+                self.assertEqual (-1, calc(1,0.1))
+
+        def test_invalid_strnum_2 (self):
+                self.assertEqual (-1, calc("1",2))
+
+        def test_invalid_2_strnum (self):
+                self.assertEqual (-1, calc(2,"1"))
 
